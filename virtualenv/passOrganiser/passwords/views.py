0afcc6019c33704b979cd526f7007646216d8cce
Daniel Aves <sc16da@leeds.ac.uk>
2020-08-15 19:13:35 +0100

    Inital project setup with gitignore and relevant Django files

---
+++ virtualenv/passOrganiser/passwords/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def LoginPage(request):
+    context = {}
+    return HttpResponse("Page loaded successfully")
+
+
+# Create your views here.
