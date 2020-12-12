d00ad85b12208e3a82bd110b37773118caefae87
Daniel Aves <sc16da@leeds.ac.uk>
2020-12-12 17:31:55 +0000

    Entered passwords on main page can now be encrypted

--- virtualenv/passOrganiser/passwords/views.py
+++ virtualenv/passOrganiser/passwords/views.py
@@ -9,13 +9,13 @@ from cryptography.fernet import Fernet, InvalidToken
 from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-#from .forms import CreateUserForm
+from .forms import CreateUserForm
 from django.contrib.auth import authenticate, login, logout
 import os
 
 
 def LoginPage(request):
-    if request.method == 'POST': 
+    if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         print(username, password)
@@ -25,7 +25,10 @@ def LoginPage(request):
         if user is not None: 
               login(request, user)
               print("Success")
-              ##redirect('home')
+              return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            return render(request, 'passwords/login.html')
     return render(request, 'passwords/login.html')
 
 def HomePage(request):
@@ -49,19 +52,25 @@ def Register(request):
     return render(request, 'passwords/register.html',context)
 
 def SavedPassword(request):
-    passwords = Password.objects.all()
-
-    print("here in saved passwords")
 
     if request.method == 'POST':
-        print("here in post request method")
-        site = request.POST.get('site')
-        print(site)
-        print(request.POST.get('site'))
-        print(request.POST.get('username'))
-        print(request.POST.get('password'))
+        password = request.POST.get('password')
+        encrypted = encryptPassword(request,password)
+        #print(encrypted)
+        passwords = Password.objects.all()
+        return redirect(SavedPassword)
+    passwords = Password.objects.all()
 
-        
+
+    # if request.method == 'POST':
+    #     print("here in post request method")
+    #     site = request.POST.get('site')
+    #     print(site)
+    #     print(request.POST.get('site'))
+    #     print(request.POST.get('username'))
+    #     print(request.POST.get('password'))
+
+       
     currentUser = request.user 
     #print(request.user.password)
 
@@ -91,32 +100,35 @@ def storePass(request):
 
 
 
-def encryptPassword(request,password):
+def encryptPassword(request,enteredPassword):
+
+
+    # if request.method == 'POST':
+    #     site = request.POST.get('site')
+    #     username = request.post.get('username')
+    #     enteredPassword = request.POST.get('password')
 
     currentUser = request.user 
-    #enteredPassword = request.user.password
-    enteredPassword = "UsersloginPass2"
-    # print(enteredPassword)
-    # print(password)
-    enteredPassword = enteredPassword.encode()
+    #enteredPassword = "UsersloginPass2"
+
+    #enteredPassword = enteredPassword.encode()
 
     salt = "b'\xd9\xe2\x1eS\x17\x15e\xee\x85\xe5\xac\xfb\xe4o\x9d\x05'".encode()
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
         salt=salt,
-        iterations=100000,
+        iterations=100000, 
         backend=default_backend()
     )
     #Method of getting key based on entered password
     #key = base64.urlsafe_b64encode(kdf.derive(enteredPassword))
-    print(enteredPassword)
-    print(settings.SECRET_KEY)
+
     secretKey = settings.SECRET_KEY.encode()
     key = base64.urlsafe_b64encode(kdf.derive(secretKey))
     f = Fernet(key) 
-    encrypted = f.encrypt(password.encode())
-    print(encrypted)
+    encrypted = f.encrypt(enteredPassword.encode())
+
 
 
     #encrypted = "gAAAAABfRpB2D1NWxQds0xPwSJCoa9w60IX0YLIDCRwJbj1EWD0EPG9y1KiBNQ5dob1knWE0LhceFUNaAxrGi7rXlrKxgxVYHLTg02Nst8d6WwFAHub3M1Q=".encode()
@@ -130,3 +142,6 @@ def encryptPassword(request,password):
         print("Invalid Key - Unsucessfully decrypted")
 
     print(decrypted.decode())
+
+
+    return encrypted
